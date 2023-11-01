b355d3b6f7feb8fac9b14a7ae51303840ce409f5
hendricksonweib <hendricksonrocha22@gmail.com>
2023-11-01 10:42:40 -0300

    :bug: build localStorage
    Signed-off-by: hendricksonweib <hendricksonrocha22@gmail.com>

--- script.js
+++ script.js
@@ -1,71 +1,102 @@
-// Função para adicionar um produto ao carrinho
-function adicionarAoCarrinho(id) {
-    // Recupere os dados do produto da seção correspondente no index.html
-    const produto = document.querySelector(`#produto${id}`)
+const btnPlaystation = document.getElementById('btnplaystation')
+const btnTablet = document.getElementById('btntablet')
+const btnPlaca = document.getElementById('btnplaca')
+const btnGabinete = document.getElementById('btngabinete')
+const btnNotebook = document.getElementById('btnnotebook')
+const btnSsd = document.getElementById('btnssd')
+const btnAll = document.getElementById('btnallinone')
+const btnMonitor = document.getElementById('btnmonitor')
+const btnFonte = document.getElementById('btnfonte')
 
-    // Clone o elemento da seção
-    const produtoClonado = produto.cloneNode(true);
+btnPlaystation.addEventListener('click',function() {
+  if(btnPlaystation.textContent === 'Adicionar ao carrinho'){
+    btnPlaystation.textContent = 'Produto adicionado !'
+    localStorage.setItem('playstation','produto 1')
+  } else{
+    btnPlaystation.textContent = 'Adicionar ao carrinho'
+    localStorage.removeItem('playstation','produto 1')
+  }
+})
+btnTablet.addEventListener('click',function() {
+  if(btnTablet.textContent === 'Adicionar ao carrinho'){
+    btnTablet.textContent = 'Produto adicionado !'
+    localStorage.setItem('tablet','produto 2')
+  } else{
+    btnTablet.textContent = 'Adicionar ao carrinho'
+    localStorage.removeItem('tablet','produto 2')
+  }
+})
+btnPlaca.addEventListener('click',function() {
+  if(btnPlaca.textContent === 'Adicionar ao carrinho'){
+    btnPlaca.textContent = 'Produto adicionado !'
+    localStorage.setItem('placa de video','produto 3')
+  } else{
+    btnPlaca.textContent = 'Adicionar ao carrinho'
+    localStorage.removeItem('placa de video','produto 3')
+  }
+})
+btnGabinete.addEventListener('click',function() {
+  if(btnGabinete.textContent === 'Adicionar ao carrinho'){
+    btnGabinete.textContent = 'Produto adicionado !'
+    localStorage.setItem('gabinete','produto 4')
+  } else{
+    btnGabinete.textContent = 'Adicionar ao carrinho'
+    localStorage.removeItem('gabinete','produto 4')
+  }
+})
+btnNotebook.addEventListener('click',function() {
+  if(btnNotebook.textContent === 'Adicionar ao carrinho'){
+    btnNotebook.textContent = 'Produto adicionado !'
+    localStorage.setItem('notebook','produto 5')
+  } else{
+    btnNotebook.textContent = 'Adicionar ao carrinho'
+    localStorage.removeItem('notebook','produto 5')
+  }
+})
+btnSsd.addEventListener('click',function() {
+  if(btnSsd.textContent === 'Adicionar ao carrinho'){
+    btnSsd.textContent = 'Produto adicionado !'
+    localStorage.setItem('ssd','produto 6')
+  } else{
+    btnSsd.textContent = 'Adicionar ao carrinho'
+    localStorage.removeItem('ssd','produto 6')
+  }
+})
+btnAll.addEventListener('click',function() {
+  if(btnAll.textContent === 'Adicionar ao carrinho'){
+    btnAll.textContent = 'Produto adicionado !'
+    localStorage.setItem('all in one','produto 7')
+  } else{
+    btnAll.textContent = 'Adicionar ao carrinho'
+    localStorage.removeItem('all in one','produto 7')
+  }
+})
+btnMonitor.addEventListener('click', function() {
+   if(btnMonitor.textContent === 'Adicionar ao carrinho'){
+    btnMonitor.textContent = 'Produto adicionado !'
+    localStorage.setItem('monitor','produto 8')
+   }else{
+    btnMonitor.textContent = 'Adicionar ao carrinho'
+    localStorage.removeItem('monitor','produto 8')
+   }
+})
+btnFonte.addEventListener('click',function() {
+  if(btnFonte.textContent === 'Adicionar ao carrinho'){
+    btnFonte.textContent = 'Produto adicionado !'
+    localStorage.setItem('fonte','produto 9')
+  } else{
+    btnFonte.textContent = 'Adicionar ao carrinho'
+    localStorage.removeItem('fonte','produto 9')
+  }
+})
 
-    // Altere o ID do botão para evitar duplicações no carrinho
-    const botao = produtoClonado.querySelector(".car")
-    botao.id = `btnremover${id}`
-    botao.textContent = "Remover do Carrinho"
+const sidebar = document.getElementById('expandir')
+const carshop = document.getElementById('sidebar')
+const back = document.getElementById('backHome')
 
-    // Adicione o produto clonado ao carrinho.html
-    const carrinho = document.getElementById("carrinho")
-    carrinho.appendChild(produtoClonado)
-
-    // Defina o botão para remover o produto do carrinho
-    botao.addEventListener("click", () => {
-        carrinho.removeChild(produtoClonado)
-        localStorage.removeItem(`produto${id}`)
-    });
-
-    // Salve os dados do produto no localStorage para que possam ser restaurados após o recarregamento
-    localStorage.setItem(`produto${id}`, produtoClonado.outerHTML)
-
-    // Desative o botão Adicionar ao Carrinho no index.html
-    const botaoAdicionar = document.querySelector(`#btn${id}`)
-    botaoAdicionar.disabled = true
-
-
-// Função para inicializar o carrinho na página carrinho.html
-function inicializarCarrinho() {
-    const carrinho = document.getElementById("carrinho")
-
-    // Percorra os itens no localStorage e adicione-os ao carrinho
-    for (let i = 0; i < localStorage.length; i++) {
-        const key = localStorage.key(i)
-        if (key.startsWith("produto")) {
-            const produtoHTML = localStorage.getItem(key)
-            const produtoClonado = document.createElement("div")
-            produtoClonado.innerHTML = produtoHTML
-
-            // Altere o ID do botão para o botão de Remover
-            const botao = produtoClonado.querySelector(".car")
-            const id = key.replace("produto", "")
-            botao.id = `btnremover${id}`;
-            botao.textContent = "Remover do Carrinho"
-            // Adicione o produto clonado ao carrinho
-            carrinho.appendChild(produtoClonado)
-
-            // Defina o botão para remover o produto do carrinho
-            botao.addEventListener("click", () => {
-                carrinho.removeChild(produtoClonado);
-                localStorage.removeItem(key)
-            });
-        }
-    }
-}
-
-// Adicione manipuladores de eventos para os botões Adicionar ao Carrinho no index.html
-for (let i = 1; i <= 9; i++) {
-    const botaoAdicionar = document.querySelector(`#btn${i}`)
-    botaoAdicionar.addEventListener("click", () => adicionarAoCarrinho(i))
-}
-
-// Chame a função de inicialização no carrinho.html
-if (document.getElementById("carrinho")) {
-    inicializarCarrinho()
-}
-}
+carshop.addEventListener('click', function(){
+    sidebar.style.display = 'block'
+    back.addEventListener('click', function(){
+      sidebar.style.display = 'none'
+    })
+})
